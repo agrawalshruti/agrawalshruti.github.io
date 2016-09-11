a4881b8eea077f17fa730f6494475e04ec38bd19
Willem Dehaes <willemdehaes@Willems-MacBook-Pro.local>
2016-09-11 15:55:03 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// grab an element
+var myElement = document.querySelector(".top-nav");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init();
+console.log("test");
