279f3e6269b10e6a67ba7178d60e72ecf5eb3d67
Willem Dehaes <willemdehaes@gmail.com>
2016-09-16 13:31:25 +0200

    switched from getskeleton to html5 responsive boilerplate

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-// grab an element
-var myElement = document.querySelector("#top-nav");
-// construct an instance of Headroom, passing the element
-var headroom  = new Headroom(myElement);
-// initialise
-headroom.init();
-console.log("test");
+
