b68121e206bbdd483b98109c3999a2e07c3b1d9c
Willem Dehaes <willem@datacamp.com>
2017-02-08 20:06:30 +0100

    finish header and add to all work pages

--- js/work.js
+++ js/work.js
@@ -1,24 +1,67 @@
 $(document).ready(function() {
 
+  function checkBackgroundColor(color) {
+
+  }
+
   function updateWorkNavigation() {
      workSections.each(function(){
       $this = $(this);
-      var activeWorkNavTitle = $('.work-nav-title[data-number="'+$this.attr('id').slice(-1) +'"]');
-      var activeWorkNavIndex = $('.work-nav-index[data-number="'+$this.attr('id').slice(-1) +'"]');
+      var dataNumber = $this.attr('id').slice(-1);
+      var activeWorkNavTitle = $('.work-nav-title[data-number="'+ dataNumber +'"]');
+      var activeWorkNavIndex = $('.work-nav-index[data-number="'+ dataNumber +'"]');
       if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
         activeWorkNavTitle.removeClass('hidden');
         activeWorkNavIndex.addClass('work-nav-index-active');
+        checkBackgroundColor(dataNumber);
       }else {
         activeWorkNavTitle.addClass('hidden');
         activeWorkNavIndex.removeClass('work-nav-index-active');
       }
     });
   }
+
+  function hoverNavIndex(dataNumber) {
+    workNavTitles.each(function(){
+      $this = $(this);
+      if ($this.data("number") === dataNumber) {
+        $this.removeClass('hidden');
+
+      }
+      else {
+        $this.addClass('hidden');
+      }
+    })
+
+    workNavIndices.each(function() {
+       $this = $(this);
+      if ($this.data("number") === dataNumber) {
+        $this.addClass('work-nav-index-active');
+
+      }
+      else {
+        $this.removeClass('work-nav-index-active');
+      }
+    })
+  }
+
   var workSections = $('.work-section');
   var workNavSections = $('#work-nav');
   var workNavTitles = $('.work-nav-title');
+  var workNavIndices = $('.work-nav-index');
   $(window).on('scroll', function() {
     updateWorkNavigation();
-  })
+  });
+
+  $(workNavIndices).hover(
+    function() {
+      hoverNavIndex($(this).data("number"))
+      // console.log($(this).data("number"))
+    },
+    function() {
+      updateWorkNavigation();
+    }
+    )
+
   updateWorkNavigation();
 })
